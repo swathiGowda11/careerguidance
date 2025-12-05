3fa50bcb7ef87460f48172d1f8c18eaa4f130a92
swathiGowda11 <151748189+swathiGowda11@users.noreply.github.com>
2025-12-05 21:09:14 +0530

    Change st.write to st.dataframe for skill data

--- careerguidance.py
+++ careerguidance.py
@@ -143,7 +143,7 @@ plt.show()
 
 # Calculate the frequency of each skill within each suggested career
 skill_career_crosstab = pd.crosstab(df['Suggested_Career'], df['Skills'])
-st.write(skill_career_crosstab)
+st.dataframe(skill_career_crosstab)
 
 # Plotting the stacked bar chart
 plt.figure(figsize=(8, 5))
